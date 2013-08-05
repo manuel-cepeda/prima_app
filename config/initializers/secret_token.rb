40f01c12ce24b184e0442a29e1ff485e93ac0543
manuel-cepeda <manuel.cepeda@mavae.cl>
2013-08-05 18:00:20 -0400

    primer repositorio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PrimaApp::Application.config.secret_key_base = '1b8ca1135f3283b4dd2fef1bec3041e65903e7365ed4065163dad88420fcafbc0209acda251bda4abb51018f54eaa21f071fc7513ad847e41e7fbe9ed4e40f73'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PrimaApp::Application.config.session_store :cookie_store, key: '_prima_app_session'
