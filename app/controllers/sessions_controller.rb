1ac5509abcc8fb8ee2ef38e6df66a17e4bca718c
manuel-cepeda <manuel.cepeda@mavae.cl>
2013-08-22 17:50:06 -0400

    test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,35 +1,11 @@
 class SessionsController < ApplicationController
     def new
 
-         if params[:code]
-            # acknowledge code and get access token from FB
-            session[:access_token] = session[:oauth].get_access_token(params[:code])
-        end 
- 
-        # auth established, now do a graph call:
-        @api = Koala::Facebook::API.new(session[:access_token])
- 
-        begin
-            @user_profile = @api.get_object(current_user.fb_id)
-        rescue Exception=>ex
-            puts ex.message
-            #if user is not logged in and an exception is caught, redirect to the page where logging in is requested
-            redirect_to '/logins' and return
-        end
- 
-        respond_to do |format|
-         format.html {   }   
-        end
+
 
     end
 
 
-    def logins
-        session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + '/')
-        @auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"read_stream publish_stream") 
-
-        redirect_to @auth_url
-    end
 
 
 
@@ -55,13 +31,7 @@ class SessionsController < ApplicationController
 	 	
 	  end
 
-       #say friend is a new user
-      if session["fb_access_token"].present?
-          @graph = Koala::Facebook::API.new(session["fb_access_token"])
 
-          profile = @graph.get_object("me")
-          @graph.put_connections("me", "feed", :message => "#{current_user.name} es un nuevo usuario en Crowdly! #{root_url}")
-      end
 
 	  redirect_to root_url, :success => "Signed in!"
 
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,3 @@
 APP_ID= '708728955810882' # please change!
 APP_SECRET= 'd1a3acb461c828ae01a2b6f33951ba8d' # please change!
-SITE_URL = 'http://localhost:3000/' # please change!
+SITE_URL = 'http://www.krowdly.com/' # please change!
