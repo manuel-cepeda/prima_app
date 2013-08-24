7614ac27f68132922e2958365add3e3ad8b6832f
manuel-cepeda <manuel.cepeda@mavae.cl>
2013-08-24 16:40:30 -0400

    alpha

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,10 +3,10 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
 
   if Rails.env.development?
-    provider :facebook, '1417372111822319', '6c92d321b924e46e81d2045c7b2a2177', { :scope => "publish_stream"}
+    provider :facebook, '1417372111822319', '6c92d321b924e46e81d2045c7b2a2177', { :scope => "email, publish_stream"}
 
   else
-	provider :facebook, '708728955810882', 'd1a3acb461c828ae01a2b6f33951ba8d', { :scope => "publish_stream"}
+	provider :facebook, '708728955810882', 'd1a3acb461c828ae01a2b6f33951ba8d', { :scope => "email, publish_stream"}
 
   end
 end
