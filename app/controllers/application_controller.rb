dc4364770fbef6a85fef8913557b1a7d8a3c4301
manuel-cepeda <manuel.cepeda@mavae.cl>
2013-08-30 12:37:52 -0400

    mobile 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  include Mobylette::RespondToMobileRequests
+
   include SessionsHelper
 
-	mobylette_config do |config|
-	  config[:skip_xhr_requests] = false
-	end
+
 
 end
