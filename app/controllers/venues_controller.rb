cd0fce40557f752866da084a6eb3533bdbfbe877
manuel-cepeda <manuel.cepeda@mavae.cl>
2013-09-28 17:29:59 -0300

    pictures facebook

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -98,7 +98,7 @@ class VenuesController < ApplicationController
           @graph = Koala::Facebook::API.new(session["fb_access_token"])
 
           profile = @graph.get_object("me")
-          @graph.put_connections("me", "feed", :message => "Lo está pasando bien en #{@venue.title}", :link => "#{root_url}")
+          @graph.put_connections("me", "feed", :message => "Lo está dando todo en #{@venue.title}", :link => "#{root_url}", :picture => "/good.png")
 
       end
 
@@ -121,9 +121,7 @@ class VenuesController < ApplicationController
           @graph = Koala::Facebook::API.new(session["fb_access_token"])
 
           profile = @graph.get_object("me")
-
-          @graph.put_connections("me", "feed", :message => "Lo está pasando bien en #{@venue.title}", :link => "#{root_url}", :picture => "http://aquacard.hu/ferencvaros/sites/default/files/pics/boldog%20%C3%A9let.jpg")
-      #    @graph.put_connections("me", "feed", :message => "Le gustaría estar en un lugar más entretenido que #{@venue.title}", :link => "#{root_url}")
+          @graph.put_connections("me", "feed", :message => "Está dando la hora en #{@venue.title}", :link => "#{root_url}", :picture => "/bad.png")
       end
 
       #current_user.vote_against(@venue = Venue.find(params[:id]))
