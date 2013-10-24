0db047f7dd55b928736a8a80ef8958bb68ad9782
manuel-cepeda <manuel.cepeda@mavae.cl>
2013-10-24 18:01:04 -0300

    hola

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,26 +1,8 @@
 class RatingsController < ApplicationController
-  include UsersHelper
+
   def update
     @rating = Rating.find(params[:id])
     @venue= @rating.venue
-    @user= @rating.user
-
-
-
-
-			 #send information to user facebook 
-	      if session["fb_access_token"].present?
-	          @graph = Koala::Facebook::API.new(session["fb_access_token"])
-
-	          profile = @graph.get_object("me")
-	          @graph.put_connections("me", "feed", :message => "Voto que #{@venue.title} #{params[:score]}", :link => "#{root_url}", :picture => "http://www.krowdly.com/assets/good.png")
-
-	      end
-
-
-
-
-
     if @rating.update_attributes(score: params[:score])
       respond_to do |format|
         format.js
