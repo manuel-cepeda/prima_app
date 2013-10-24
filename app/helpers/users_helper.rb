04bf0fdc8b1530a980738b4f0b5f46bd0709503f
manuel-cepeda <manuel.cepeda@mavae.cl>
2013-10-24 13:13:26 -0300

    fix facebook

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,22 +7,6 @@ module UsersHelper
 		image_tag(picture_url, alt: user.name, class: "avatar")
 	end
 
-	def phrases(score)
 
-			case score # a_variable is the variable we want to compare
-			when 1    #compare to 1
-			  "it was 1" 
-			when 2    #compare to 2
-			  "it was 2"
-			when 3    #compare to 2
-			  "it was 2"
-			when 4    #compare to 2
-			  "it was 2"
-			when 5    #compare to 2
-			  "it was 2"
-			else
-			  "it was something else"
-			end
-	end
 
 end
