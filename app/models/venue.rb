56e95ab7f7c76d4fcc1a16a42635e3105f57d40e
manuel-cepeda <manuel.cepeda@mavae.cl>
2014-02-28 14:52:33 -0300

    Add user following

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -42,9 +42,10 @@ class Venue < ActiveRecord::Base
 	    sheet1 = book.worksheet 0
 	    
 	    sheet1.each do |row|
-			
+			user=User.find(1)
 	        #add venues from excel
-	      Venue.create(:title => "#{row[0]}", :address => "#{row[1]}, #{row[4]},Santiago, Chile")
+	        user.venues.build(:title => "#{row[0]}", :address => "#{row[1]}, #{row[4]},Santiago, Chile")
+	     
 		  #Not overload Google API
 	      sleep 1     
 	    end	
