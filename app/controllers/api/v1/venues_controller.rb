5dbcd73572ab3e60900dc8c2da8ee5194b0dbd54
manuel-cepeda <manuel.cepeda@mavae.cl>
2014-02-28 19:32:37 -0300

    Add user following

--- app/controllers/api/v1/venues_controller.rb
+++ app/controllers/api/v1/venues_controller.rb
@@ -30,7 +30,7 @@ class Api::V1::VenuesController < ApplicationController
      AVG(ratings.score) as average")
     .where('ratings.updated_at > ?', 5.hours.ago)
     .group("venues.id, venues.title")
-    .order("average DESC").limit(10)
+    .order("average DESC").limit(12)
 
     respond_with @venues
 
